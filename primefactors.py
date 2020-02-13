dd126b46fce887d8145e7e835b31cca9d9c9198c
paras <parasjitaliya@gmail.com>
2020-02-13 15:44:26 +0530

    Added:print prime factor for the number

---
+++ primefactors.py
@@ -0,0 +1,9 @@
+import Utility
+
+print("---------welcom in to prime factorization-----------")
+#take user input
+
+number = int(input("Enter the number for prime factor :"))
+
+#function calling
+print(Utility.primefactor(number))
