c6b72bc18bcbc2d5d2d7b5cabb910e080032ad85
paras <parasjitaliya@gmail.com>
2020-02-14 12:02:51 +0530

    Added:Quadratic

---
+++ Quadratic.py
@@ -0,0 +1,8 @@
+import Utility
+
+a = int(input("Enter the coefficients of a: "))
+b = int(input("Enter the coefficients of b: "))
+c = int(input("Enter the coefficients of c: "))
+
+Utility.quadratic(a, b, c)
+
